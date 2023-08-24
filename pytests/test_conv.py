c6564326768c6f1c31f17f59bf409196a8602c5d
simonynwa <dingyuang@sjtu.edu.cn>
2023-08-24 22:11:33 +0800

    modify

--- pytests/test_conv.py
+++ pytests/test_conv.py
@@ -9,9 +9,9 @@ class CNN(torch.nn.Module, kqinn.KQI):
     def __init__(self) -> None:
         super().__init__()
         self.layers1 = kqinn.Sequential(
-            # 1x28x28
-            kqinn.Conv2d(in_channels=1, out_channels=2, kernel_size=3, stride=1, padding=0, dilation=1, bias=False),
-            # 2x26*26
+            # 1x26x26
+            kqinn.Conv2d(in_channels=1, out_channels=2, kernel_size=3, stride=1, padding=1, dilation=1, bias=False),
+            # 2x26x26
             kqinn.Conv2d(in_channels=2, out_channels=3, kernel_size=3, stride=3, padding=0, dilation=2, bias=False),
         )
         self.layers2 = kqinn.Sequential(
@@ -48,11 +48,16 @@ class CNN(torch.nn.Module, kqinn.KQI):
 def true_kqi():
     G = kqitool.DiGraph()
     for i,j in itertools.product(range(28), range(28)):
-        G.add_node(f'L1_{i}-{j}', [])
+        if 0<=i<=26 and 0<=j<=26:
+            G.add_node(f'L1_{i}-{j}', [])  # 1x26x26
+        else:
+            G.add_node(f'Padding_{i}-{j}',[]) # Padding=1
+
     for i,j in itertools.product(range(26), range(26)):
         preds = [f'L1_{k1}-{k2}' for k1, k2 in itertools.product([i, i+1, i+2], [j, j+1, j+2])]
         G.add_node(f'L2_{i}-{j}_1', preds)
         G.add_node(f'L2_{i}-{j}_2', preds)
+    
     for i,j in itertools.product(range(8), range(8)):
         preds = [f'L2_{k1}-{k2}_{k3}' for k1, k2 in itertools.product([i*3, i*3+2, i*3+4], [j*3, j*3+2, j*3+4]) for k3 in [1,2]]
         G.add_node(f'L3_{i}-{j}_1', preds)
@@ -76,13 +81,17 @@ def true_kqi():
     kqi += sum(map(lambda k: G.kqi(k) if "L2_" in k else 0, G.nodes()))
     logging.debug(f'L2: KQI={kqi}, node={len([k for k in G.nodes() if "L2_" in k])}, volume={sum([G.volume(k) for k in G.nodes() if "L2_" in k])}')
     kqi += sum(map(lambda k: G.kqi(k) if "L1_" in k else 0, G.nodes()))
-    logging.debug(f'L1: KQI={kqi}, node={len([k for k in G.nodes() if "L1_" in k])}, volume={sum([G.volume(k) for k in G.nodes() if "L1_" in k])}')
+    logging.debug(f'L1: KQI={kqi}, node={len([k for k in G.nodes() if "L1_" in k])}, volume={sum([G.volume(k) for k in G.nodes() if "L1_" in k])}') 
+
+    kqi += sum(map(lambda k: G.kqi(k) if "Padding_" in k else 0, G.nodes()))
+    logging.debug(f'Padding: KQI={kqi}, node={len([k for k in G.nodes() if "Padding_" in k])}, volume={sum([G.volume(k) for k in G.nodes() if "Padding_" in k])}')
+    
     logging.debug(f'Total volume = {G.graph_volume()}')
     return sum(map(lambda k: G.kqi(k), G.nodes()))
 
 
 def test():
-    kqi = CNN().KQI(torch.randn(1,28,28))
+    kqi = CNN().KQI(torch.randn(1,26,26))
 
     true = true_kqi()
     logging.debug(f'KQI = {kqi} (True KQI = {true})')
