71cea801f32b9a03bdaec736f386152ee98c6a2c
Betty Huang <bettyhuang@BettydeMacBook-Air.local>
2023-08-30 15:38:45 +0800

    test_pooling_PASSED

--- kqinn/pooling.py
+++ kqinn/pooling.py
@@ -1,12 +1,21 @@
 import torch
 import numpy as np
+import logging
+import itertools
 
 from .kqi import KQI
 
 
 class MaxPool2d(torch.nn.MaxPool2d, KQI):
     def KQIforward(self, x: torch.Tensor) -> torch.Tensor:
-        return super().KQIforward(x)
+        kernel_size = self.kernel_size if isinstance(self.kernel_size, tuple) else (self.kernel_size, self.kernel_size)
+
+        self.input_size = x.shape
+        x_new = self.forward(x)
+        assert x_new.shape[-3] == x.size(0)
+        # padding not considered
+        KQI.W += np.prod(x_new.shape) * np.prod(kernel_size)
+        return x_new
     
 
     def KQIbackward(self, volume: torch.Tensor, volume_backward: torch.Tensor = None) -> torch.Tensor:
