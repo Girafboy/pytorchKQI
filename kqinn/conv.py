1221498f7d1f1b13a74338f414158e4f6962f83e
simonynwa <dingyuang@sjtu.edu.cn>
2023-09-01 11:43:15 +0800

    conv

--- kqinn/conv.py
+++ kqinn/conv.py
@@ -16,8 +16,12 @@ class Conv2d(torch.nn.Conv2d, KQI):
             padding_values = (self.padding[1],self.padding[1],self.padding[0],self.padding[0])
             x_padded = torch.nn.functional.pad(x, padding_values)
             x_new = self.forward(x_padded)
-
+        
             KQI.W += np.prod(x_new.shape) * np.prod(self.kernel_size) * self.in_channels
+            
+            connections_to_padding = np.prod(self.kernel_size) * self.in_channels * self.padding[0] * self.padding[1]
+            KQI.W -= connections_to_padding
+
         else:
             x_new = self.forward(x)
             KQI.W += np.prod(x_new.shape) * np.prod(self.kernel_size) * self.in_channels
@@ -27,17 +31,15 @@ class Conv2d(torch.nn.Conv2d, KQI):
 
     def KQIbackward(self, volume: torch.Tensor, volume_backward: torch.Tensor = None) -> torch.Tensor:
         if self.padding[0] or self.padding[1]:
-            volumes_new = torch.zeros(self.input_size)
-            _, H, W = volume.shape
-            original_i = range(-self.padding[0], self.kernel_size[0]*self.dilation[0]-self.padding[0], self.dilation[0])
-            padded_i = [value for value in original_i if 0 <= value <= H]
-            original_j = range(-self.padding[1], self.kernel_size[1]*self.dilation[1]-self.padding[1], self.dilation[1])
-            padded_j = [value for value in original_j if 0 <= value <= W]
+            if volume_backward is None:
+                volume_backward = torch.zeros(self.input_size)
+                _, H, W = volume.shape
             
-            for c,i,j in itertools.product(range(self.in_channels), padded_i, padded_j):
-                volumes_new[c, i:H*self.stride[0]+i:self.stride[0], j:W*self.stride[1]+j:self.stride[1]] += self.out_channels + (volume / np.prod(self.kernel_size) / self.in_channels).sum(dim=0)
-            for c,i,j in itertools.product(range(self.in_channels), padded_i, padded_j):
-                kqi += self.KQI_formula((volume[0] / np.prod(self.kernel_size) / self.in_channels), volumes_new[c, i:H*self.stride[0]+i:self.stride[0], j:W*self.stride[1]+j:self.stride[1]]) * self.out_channels
+                for c, i, j in itertools.product(range(self.in_channels),range(-self.padding[0], (H-self.kernel_size[0]+1)*self.stride[0]+1, self.dilation[0]),range(-self.padding[1], (W-self.kernel_size[1]+1)*self.stride[1]+1, self.dilation[1])):
+                    volume_backward[c, max(0, i):min(H, i+self.kernel_size[0]*self.dilation[0]):self.stride[0], max(0, j):min(W, j+self.kernel_size[1]*self.dilation[1]):self.stride[1]] += self.out_channels + (volume / np.prod(self.kernel_size) / self.in_channels).sum(dim=0)
+                    
+            for c, i, j in itertools.product(range(self.in_channels),range(-self.padding[0], (H-self.kernel_size[0]+1)*self.stride[0]+1, self.dilation[0]),range(-self.padding[1], (W-self.kernel_size[1]+1)*self.stride[1]+1, self.dilation[1])):
+                KQI.kqi += self.KQI_formula((volume[0] / np.prod(self.kernel_size) / self.in_channels),volume_backward[c, max(0, i):min(H, i+self.kernel_size[0]*self.dilation[0]):self.stride[0], max(0, j):min(W, j+self.kernel_size[1]*self.dilation[1]):self.stride[1]]) * self.out_channels
         else:
             if volume_backward is None:
                 volume_backward = torch.zeros(self.input_size)
