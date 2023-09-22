af25563de1965f51185ad336cb576f1788d56b5d
Betty Huang <bettyhuang@BettydeMacBook-Air.local>
2023-09-22 18:44:20 +0800

    edited

--- kqinn/pooling.py
+++ kqinn/pooling.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import logging
 import itertools
+import math
 
 from .kqi import KQI
 
@@ -18,9 +19,11 @@ class MaxPool2d(torch.nn.MaxPool2d, KQI):
         assert x_new.shape[-3] == x.size(0)
 
         if padding[0] or padding[1]:
-            C_new, H_new, W_new = x_new.shape
+            _, H_new, W_new = x_new.shape
+            _, H, W = x.shape
 
             degree = torch.zeros((x.shape[0],x.shape[1]+2*padding[0],x.shape[2]+2*padding[1]))
+
             for c, i, j in itertools.product(range(x.size(0)),
                                              range(0, kernel_size[0] * dilation[0], dilation[0]),
                                              range(0, kernel_size[1] * dilation[1], dilation[1])):
@@ -37,7 +40,8 @@ class MaxPool2d(torch.nn.MaxPool2d, KQI):
         return x_new
 
     def KQIbackward(self, volume: torch.Tensor, volume_backward: torch.Tensor = None) -> torch.Tensor:
-        C, H, W = volume.shape
+        _, H, W = volume.shape
+        _, H_backward, W_backward = self.input_size
         kernel_size = self.kernel_size if isinstance(self.kernel_size, tuple) else (self.kernel_size, self.kernel_size)
         dilation = self.dilation if isinstance(self.dilation, tuple) else (self.dilation, self.dilation)
         stride = self.stride if isinstance(self.stride, tuple) else (self.stride, self.stride)
@@ -46,49 +50,34 @@ class MaxPool2d(torch.nn.MaxPool2d, KQI):
         if padding[0] or padding[1]:
             if volume_backward is None:
 
-                degree_back = torch.zeros((self.input_size[0],self.input_size[1]+2*padding[0],self.input_size[2]+2*padding[1]))
+                degree = torch.zeros(volume.shape)
                 for c, i, j in itertools.product(range(volume.size(0)),
                                                  range(0, kernel_size[0] * dilation[0], dilation[0]),
                                                  range(0, kernel_size[1] * dilation[1], dilation[1])):
-                    degree_back[c, i:H * stride[0] + i:stride[0], j:W * stride[1] + j:stride[1]] += 1
-                # Extract the real part of degree
-                degree_back[:,0:padding[0],:] = 0
-                degree_back[:, -padding[0]:-1, :] = 0
-                degree_back[:, :, 0:padding[0]] = 0
-                degree_back[:, :, -padding[0]:-1] = 0
-                degree = torch.zeros(C,H,W)
-                for c, i, j in itertools.product(range(volume.size(0)),
-                                                 range(0, kernel_size[0] * dilation[0], dilation[0]),
-                                                 range(0, kernel_size[1] * dilation[1], dilation[1])):
-                    degree[:, :, :] += degree_back[c, i:H * stride[0] + i:stride[0], j:W * stride[1] + j:stride[1]]
+                    degree[c, max(0, math.ceil((padding[0] - i) / stride[0])):min(H, (
+                                H_backward - 1 - i + padding[0]) // stride[0] + 1),max(0, math.ceil((padding[1] - j) / stride[1])):min(W, (
+                                W_backward - 1 - j + padding[1]) // stride[1] + 1)] += 1
 
-                #二维即可
                 virtual_volume_backward = torch.zeros(self.input_size[0],self.input_size[1]+2*padding[0],self.input_size[2]+2*padding[1])
                 for c, i, j in itertools.product(range(volume.size(0)),
                                                  range(0, kernel_size[0] * dilation[0], dilation[0]),
                                                  range(0, kernel_size[1] * dilation[1], dilation[1])):
-                    virtual_volume_backward[c, i:H * stride[0] + i:stride[0],
-                    j:W * stride[1] + j:stride[1]] += 1 + (volume / degree / volume.size(0)).sum(dim=0)
+                    virtual_volume_backward[c, i:H * stride[0] + i:stride[0], j:W * stride[1] + j:stride[1]] += 1 + (volume / degree / volume.size(0)).sum(dim=0)
 
                 # Extract the non-padding part of volume_backward
                 volume_backward = virtual_volume_backward[:, padding[0]:-padding[0], padding[1]:-padding[1]]
 
-            temp_volume_backward = torch.zeros(self.input_size[0], self.input_size[1] + 2 * padding[0],
-                                               self.input_size[2] + 2 * padding[1])
             for c, i, j in itertools.product(range(volume.size(0)),
                                              range(0, kernel_size[0] * dilation[0], dilation[0]),
                                              range(0, kernel_size[1] * dilation[1], dilation[1])):
-               temp_volume_backward[c, i:H * stride[0] + i:stride[0], j:W * stride[1] + j:stride[1]] = volume[c] / degree[c]
-                # =? +=? 如有多个孩子节点呢？
-                
-            temp_volume_backward[:, padding[0]:-padding[0], padding[1]:-padding[1]] = volume_backward
+                virtual_volume_backward = torch.zeros(self.input_size[0], self.input_size[1] + 2 * padding[0], self.input_size[2] + 2 * padding[1])
 
-            for c, i, j in itertools.product(range(volume.size(0)),
-                                             range(0, kernel_size[0] * dilation[0], dilation[0]),
-                                             range(0, kernel_size[1] * dilation[1], dilation[1])):
-               KQI.kqi += self.KQI_formula((volume[0] / np.prod(kernel_size)),
-                                            temp_volume_backward[c, i:H * stride[0] + i:stride[0], j:W * stride[1] + j:stride[1]])
+                virtual_volume_backward[c, i:H * stride[0] + i:stride[0], j:W * stride[1] + j:stride[1]] += volume[0] / degree[0] / volume.size(0)
 
+                virtual_volume_backward[c, i:H * stride[0] + i:stride[0], j:W * stride[1] + j:stride[1]][max(0, padding[0] - i):min(H, H - i + padding[0]),
+                max(0, padding[1] - j):min(W, W - j + padding[1])] = volume_backward[c, max(0, i - padding[0]):min(H,H + i -padding[0]), max(0, j - padding[1]):min(W,W + j -padding[1])]
+
+                KQI.kqi += self.KQI_formula((volume[0] / np.prod(kernel_size)), virtual_volume_backward[c, i:H * stride[0] + i:stride[0], j:W * stride[1] + j:stride[1]])
 
 
         else:
@@ -97,15 +86,12 @@ class MaxPool2d(torch.nn.MaxPool2d, KQI):
                 for c, i, j in itertools.product(range(volume.size(0)),
                                                  range(0, kernel_size[0] * dilation[0], dilation[0]),
                                                  range(0, kernel_size[1] * dilation[1], dilation[1])):
-                    volume_backward[c, i:H * stride[0] + i:stride[0], j:W * stride[1] + j:stride[1]] += 1 + (
-                            volume / np.prod(kernel_size) / volume.size(0)).sum(dim=0)
+                    volume_backward[c, i:H * stride[0] + i:stride[0], j:W * stride[1] + j:stride[1]] += 1 + (volume / np.prod(kernel_size) / volume.size(0)).sum(dim=0)
 
             for c, i, j in itertools.product(range(volume.size(0)),
                                              range(0, kernel_size[0] * dilation[0], dilation[0]),
                                              range(0, kernel_size[1] * dilation[1], dilation[1])):
-                KQI.kqi += self.KQI_formula((volume[0] / np.prod(kernel_size)),
-                                            volume_backward[c, i:H * stride[0] + i:stride[0],
-                                            j:W * stride[1] + j:stride[1]])
+                KQI.kqi += self.KQI_formula((volume[0] / np.prod(kernel_size)), volume_backward[c, i:H * stride[0] + i:stride[0], j:W * stride[1] + j:stride[1]])
 
         logging.debug(f'MaxPool2d: KQI={KQI.kqi}, node={np.product(volume.shape)}, volume={volume.sum()}')
         return volume_backward
