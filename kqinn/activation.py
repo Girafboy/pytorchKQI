e262adc85333eac9df1ab8bd7da7cb1e80caa77a
Liu Jialong <liujl01@sjtu.edu.cn>
2023-10-14 23:20:34 +0800

    complete multiHeadAttention

--- kqinn/activation.py
+++ kqinn/activation.py
@@ -2,7 +2,9 @@ import torch
 import numpy as np
 import logging
 
-from .kqi import KQI
+from torch import Tensor
+
+from kqinn import KQI
 
 
 class ReLU(torch.nn.ReLU, KQI):
@@ -43,4 +45,31 @@ class Softmax(torch.nn.Softmax, KQI):
         KQI.kqi += self.KQI_formula(volume / volume.shape[self.dim], volume_backward) * volume.shape[self.dim]
 
         logging.debug(f'Softmax: KQI={KQI.kqi}, node={np.prod(volume.shape)}, volume={volume.sum()}')
+        return volume_backward
+
+
+class MultiheadAttention(torch.nn.MultiheadAttention, KQI):
+    def KQIforward(self, x: torch.Tensor) -> torch.Tensor:
+        # x 为 (3, seq_len, embed_dim)
+        # 从 x 中分离出 k, q, v
+        # k, q, v 的 shape 为 (seq_len, embed_dim)
+        k, q, v = x.unbind(0)
+        KQI.W += (np.prod(k.shape) + np.prod(q.shape) + np.prod(v.shape)) * np.prod(v.shape)
+        res_for = self.forward(k, q, v)
+        # res = torch.zeros_like(v)
+        # for i in range(len(res_for)):
+        #     res += res_for[i]
+        # return res
+        return res_for[0]
+
+    def KQIbackward(self, volume: torch.Tensor, volume_backward: torch.Tensor = None) -> torch.Tensor:
+        if volume_backward is None:
+            volume_backward_ones = torch.ones((3,) + volume.shape)
+            volume_backward = (volume_backward_ones *
+                               (np.prod(volume_backward_ones.shape) + (volume / np.prod(volume.shape)).sum()))
+
+        for vol in volume_backward:
+            KQI.kqi += self.KQI_formula(volume / np.prod(volume.shape), vol)
+
+        logging.debug(f'MultiHeadAttention: KQI={KQI.kqi}, node={np.prod(volume.shape)}, volume={volume.sum()}')
         return volume_backward
--- pytests/test_multiHeadAttention.py
+++ pytests/test_multiHeadAttention.py
@@ -1,101 +1,106 @@
 import itertools
+import logging
+from typing import Tuple
 
 import torch
+from torch import Tensor
 import kqinn
 import kqitool
 
-head = 5  # 注意力头数
 
+def test_MultiHeadAttention():
+    head = 8  # 注意力头数
+    embedding_dim = 64  # 嵌入维度
+    sequence_length = 10  # 序列长度
+    head_dim = embedding_dim // head  # 每个头的维度
 
-class MHA(torch.nn.Module, kqinn.KQI):
-    def __init__(self) -> None:
-        super().__init__()
+    class TestMultiHeadAttention(torch.nn.Module, kqinn.KQI):
+        def __init__(self) -> None:
+            super().__init__()
 
-        for i in range(head):
-            setattr(self, f'layer1_Q_{i}', kqinn.Linear(in_features=5 * 5, out_features=5 * 5, bias=False))
-            setattr(self, f'layer1_K_{i}', kqinn.Linear(in_features=5 * 5, out_features=5 * 5, bias=False))
-            setattr(self, f'layer1_V_{i}', kqinn.Linear(in_features=5 * 5, out_features=5 * 5, bias=False))
-            setattr(self, f'layer2_{i}', kqinn.MatMul(featureM=5, featureN=5))
-            setattr(self, f'layer3_{i}', kqinn.Scale(feature=5 * 5, scale=1))
-            setattr(self, f'layer4_{i}', kqinn.Mask(feature=5 * 5, mask=1))
-            setattr(self, f'layer5_{i}', kqinn.SoftMax(in_features=5 * 5, out_features=5 * 5))
-            setattr(self, f'layer6_{i}', kqinn.MatMul(featureM=5, featureN=5))
+            self.layer = kqinn.MultiheadAttention(embed_dim=embedding_dim, num_heads=head)
 
-        self.layer7 = kqinn.Linear(in_features=head * 5 * 5, out_features=head * 5 * 5, bias=False)
+        def forward(self, x):
+            return self.layer(x)
 
-    # def forward(self, k, q, v):
+        def KQIforward(self, x: torch.Tensor) -> torch.Tensor:
+            return self.layer.KQIforward(x)
 
+        def KQIbackward(self, volume: torch.Tensor, volume_backward: torch.Tensor = None) -> torch.Tensor:
+            return self.layer.KQIbackward(volume)
 
-def true_kqi():
-    G = kqitool.DiGraph()
-    # 构建 Q, K, V，均为 5*5*5 的张量
-    for i, j, k in itertools.product(range(5), range(5), range(5)):
-        G.add_node(f'L1_Q_{i}-{j}-{k}', [])
-        G.add_node(f'L1_K_{i}-{j}-{k}', [])
-        G.add_node(f'L1_V_{i}-{j}-{k}', [])
+        def true_kqi(self):
+            G = kqitool.DiGraph()
+            # 构建 Q, K, V, 均为 sequence_length * embedding_dim 的张量
+            for i, j in itertools.product(range(sequence_length), range(embedding_dim)):
+                G.add_node(f'L1_Q_{i}-{j}', [])
+                G.add_node(f'L1_K_{i}-{j}', [])
+                G.add_node(f'L1_V_{i}-{j}', [])
 
-    # linear
-    # 构建 Q, K, V 的线性层，均为 head*5*5 的张量
-    # 线性层张量中的每个 5*5 的矩阵，与上一层中的一个 5*5 的矩阵全连接
-    for i in range(head):
-        preds = [f'L1_Q_{i}-{j}-{k}' for j, k in itertools.product(range(5), range(5))]
-        for j, k in itertools.product(range(5), range(5)):
-            G.add_node(f'L2_Q_{i}-{j}-{k}', preds)
-        preds = [f'L1_K_{i}-{j}-{k}' for j, k in itertools.product(range(5), range(5))]
-        for j, k in itertools.product(range(5), range(5)):
-            G.add_node(f'L2_K_{i}-{j}-{k}', preds)
-        preds = [f'L1_V_{i}-{j}-{k}' for j, k in itertools.product(range(5), range(5))]
-        for j, k in itertools.product(range(5), range(5)):
-            G.add_node(f'L2_V_{i}-{j}-{k}', preds)
+            # linear
+            for i in range(head):
+                predsQ = [f'L1_Q_{j}-{k}' for j, k in
+                          itertools.product(range(sequence_length), range(i * head_dim, (i + 1) * head_dim))]
+                predsK = [f'L1_K_{j}-{k}' for j, k in
+                          itertools.product(range(sequence_length), range(i * head_dim, (i + 1) * head_dim))]
+                predsV = [f'L1_V_{j}-{k}' for j, k in
+                          itertools.product(range(sequence_length), range(i * head_dim, (i + 1) * head_dim))]
+                for j, k in itertools.product(range(sequence_length), range(head_dim)):
+                    G.add_node(f'L2_Q_{j}-{i * head_dim + k}', predsQ)
+                    G.add_node(f'L2_K_{j}-{i * head_dim + k}', predsK)
+                    G.add_node(f'L2_V_{j}-{i * head_dim + k}', predsV)
 
-    # MatMul
-    # K, Q 线性层张量中的 5*5 矩阵相乘，形成 5*5*5 的张量 M
-    for i in range(head):
-        for j in range(5):
-            for k in range(5):
-                preds = [f'L2_Q_{i}-{j}-{l}' for l in range(5)] + [f'L2_K_{i}-{l}-{k}' for l in range(5)]
-                G.add_node(f'L3{i}-{j}-{k}', preds)
+            # MatMul
+            for i in range(head):
+                for j in range(sequence_length):
+                    for k in range(sequence_length):
+                        preds = ([f'L2_Q_{j}-{i * head_dim + m}' for m in range(head_dim)]
+                                 + [f'L2_K_{k}-{i * head_dim + m}' for m in range(head_dim)])
+                        G.add_node(f'L3_{j}-{i * sequence_length + k}', preds)
 
-    # Scale
-    # 构建 M 的放缩层，为 5*5*5 的张量
-    for i, j, k in itertools.product(range(head), range(5), range(5)):
-        preds = [f'L3{i}-{j}-{k}']
-        G.add_node(f'L4_{i}-{j}-{k}', preds)
+            # Scale
+            for i in range(head):
+                for j in range(sequence_length):
+                    for k in range(sequence_length):
+                        preds = [f'L3_{j}-{i * sequence_length + k}']
+                        G.add_node(f'L4_{j}-{i * sequence_length + k}', preds)
 
-    # Skip Mask
+            # Mask
+            # for i in range(head):
+            #     for j in range(sequence_length):
+            #         for k in range(sequence_length):
+            #             preds = [f'L4_{j}-{i * sequence_length + k}']
+            #             G.add_node(f'L5_{j}-{i * sequence_length + k}', preds)
 
-    # SoftMax
-    # 构建 M 的 SoftMax 层，为 5*5*5 的张量
-    for i in range(head):
-        preds = [f'L4_{i}-{j}-{k}' for j, k in itertools.product(range(5), range(5))]
-        for j, k in itertools.product(range(5), range(5)):
-            G.add_node(f'L5_{i}-{j}-{k}', preds)
+            # SoftMax
+            for i in range(head):
+                preds = [f'L4_{j}-{i * sequence_length + k}' for j, k in
+                         itertools.product(range(sequence_length), range(sequence_length))]
+                for j, k in itertools.product(range(sequence_length), range(sequence_length)):
+                    G.add_node(f'L6_{j}-{i * sequence_length + k}', preds)
 
-    # MatMul
-    # SoftMax 层张量与 V 线性层张量中的 5*5 矩阵相乘，形成 5*5*5 的张量 M2
-    for i in range(head):
-        for j in range(5):
-            for k in range(5):
-                preds = [f'L5_{i}-{j}-{l}' for l in range(5)] + [f'L2_V_{i}-{l}-{k}' for l in range(5)]
-                G.add_node(f'L6_{i}-{j}-{k}', preds)
+            # MatMul
+            for i in range(head):
+                for j in range(sequence_length):
+                    for k in range(head_dim):
+                        preds = ([f'L6_{j}-{i * sequence_length + m}' for m in range(sequence_length)] +
+                                 [f'L2_V_{m}-{i * head_dim + k}' for m in range(sequence_length)])
+                        G.add_node(f'L7_{j}-{i * head_dim + k}', preds)
 
-    # Linear
-    # 构建 M2 的线性层，为 head*5*5 的张量
-    preds = [f'L6_{i}-{j}-{k}' for i, j, k in itertools.product(range(head), range(5), range(5))]
-    for i, j, k in itertools.product(range(head), range(5), range(5)):
-        G.add_node(f'L7_{i}-{j}-{k}', preds)
+            # Linear
+            preds = [f'L7_{j}-{k}' for j, k in itertools.product(range(sequence_length), range(embedding_dim))]
+            for j, k in itertools.product(range(sequence_length), range(embedding_dim)):
+                G.add_node(f'L8_{j}-{k}', preds)
 
-    return sum(map(lambda m: G.kqi(m), G.nodes()))
+            return sum(map(lambda m: G.kqi(m), G.nodes()))
 
-
-def test():
-    # kqi = MHA().KQI(torch.randn(1 * 28 * 28))
-
-    true = true_kqi()
+    kqi = TestMultiHeadAttention().KQI(torch.randn(3, sequence_length, embedding_dim))
+    true = TestMultiHeadAttention().true_kqi()
     print("true_kqi: ", true)
-    # logging.debug(f'KQI = {kqi} (True KQI = {true})')
-    # assert abs(kqi - true) / true < 0.0001
+    print("kqi: ", kqi)
+    logging.debug(f'KQI = {kqi} (True KQI = {true})')
+    assert abs(kqi - true) / true < 0.0001
 
 
 if __name__ == '__main__':
-    test()
+    test_MultiHeadAttention()
