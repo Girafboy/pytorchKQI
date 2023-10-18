71eaca5d51395131da266053abf4fb4da106a4b5
Betty Huang <125963717+Betty2GitHub@users.noreply.github.com>
2023-10-18 10:34:19 +0800

    Feature/max pool2d (#6)
    * Add test_pooling
    
    * test_pooling_updated
    
    * test_pooling_updated
    
    * maxpool2d_updated
    
    * test_pooling_PASSED
    
    * Coding style updated
    
    * test_pooling updated with padding
    
    * updated with padding
    
    * corrected with padding
    
    * realized KQIforward with padding
    
    * edited
    
    * edited
    
    * editted
    
    * tidy up
    
    * MaxPool2d with padding completed
    
    * tidy up
    
    * tidy up
    
    ---------
    
    Co-authored-by: Betty Huang <bettyhuang@BettydeMacBook-Air.local>
    Co-authored-by: Huquan Kang <799385272@qq.com>

--- kqinn/pooling.py
+++ kqinn/pooling.py
@@ -1,11 +1,85 @@
 import torch
+import numpy as np
+import logging
+import itertools
+import math
+
 
 from .kqi import KQI
 
 
 class MaxPool2d(torch.nn.MaxPool2d, KQI):
     def KQIforward(self, x: torch.Tensor) -> torch.Tensor:
-        return super().KQIforward(x)
+        self.kernel_size = self.kernel_size if isinstance(self.kernel_size, tuple) else (self.kernel_size, self.kernel_size)
+        self.dilation = self.dilation if isinstance(self.dilation, tuple) else (self.dilation, self.dilation)
+        self.stride = self.stride if isinstance(self.stride, tuple) else (self.stride, self.stride)
+        self.padding = self.padding if isinstance(self.padding, tuple) else (self.padding, self.padding)
+
+        self.input_size = x.shape
+        x_new = self.forward(x)
+        assert x_new.shape[-3] == x.size(0)
+
+        if self.padding[0] or self.padding[1]:
+            degree = self._degree(x.shape, x_new.shape)
+            KQI.W += degree.sum() * x.size(0)
+        else:
+            KQI.W += np.prod(x_new.shape) * np.prod(self.kernel_size)
+
+        return x_new
 
     def KQIbackward(self, volume: torch.Tensor, volume_backward: torch.Tensor = None) -> torch.Tensor:
-        return super().KQIbackward(volume, volume_backward)
+        _, H, W = volume.shape
+
+        if self.padding[0] or self.padding[1]:
+            volume_back_padding = torch.zeros((self.input_size[0], self.input_size[1] + 2 * self.padding[0], self.input_size[2] + 2 * self.padding[1]))
+            if volume_backward is None:
+                degree = self._degree(self.input_size, volume.shape)
+                for c, i, j in itertools.product(range(volume.size(0)),
+                                                 range(0, self.kernel_size[0] * self.dilation[0], self.dilation[0]),
+                                                 range(0, self.kernel_size[1] * self.dilation[1], self.dilation[1])):
+                    volume_back_padding[c, i:H * self.stride[0] + i:self.stride[0], j:W * self.stride[1] + j:self.stride[1]] += 1 + volume[c] / degree
+                volume_backward = volume_back_padding[:, self.padding[0]:-self.padding[0], self.padding[1]:-self.padding[1]].clone()
+
+            for c, i, j in itertools.product(range(volume.size(0)),
+                                             range(0, self.kernel_size[0] * self.dilation[0], self.dilation[0]),
+                                             range(0, self.kernel_size[1] * self.dilation[1], self.dilation[1])):
+                tmp = volume_back_padding.clone()
+                H_index = torch.LongTensor([k for k in range(i, H * self.stride[0] + i, self.stride[0])])
+                W_index = torch.LongTensor([k for k in range(j, W * self.stride[1] + j, self.stride[1])])
+                x, y = torch.meshgrid(H_index, W_index)
+                tmp[c, x, y] = volume[c] / degree
+                H_index = torch.LongTensor([k for k in range(i, H * self.stride[0] + i, self.stride[0]) if k >= self.padding[0] and k < volume_back_padding.shape[1] - self.padding[0]])
+                W_index = torch.LongTensor([k for k in range(j, W * self.stride[1] + j, self.stride[1]) if k >= self.padding[1] and k < volume_back_padding.shape[2] - self.padding[1]])
+                x_padding, y_padding = torch.meshgrid(H_index, W_index)
+                tmp[c, x_padding, y_padding] = volume_back_padding[c, x_padding, y_padding]
+                KQI.kqi += self.KQI_formula(volume[c] / degree, tmp[c, x, y])
+
+        else:
+            if volume_backward is None:
+                volume_backward = torch.zeros(self.input_size)
+                for c, i, j in itertools.product(range(volume.size(0)),
+                                                 range(0, self.kernel_size[0] * self.dilation[0], self.dilation[0]),
+                                                 range(0, self.kernel_size[1] * self.dilation[1], self.dilation[1])):
+                    volume_backward[c, i:H * self.stride[0] + i:self.stride[0], j:W * self.stride[1] + j:self.stride[1]] += 1 + volume[c] / np.prod(self.kernel_size) / volume.size(0)
+
+            for c, i, j in itertools.product(range(volume.size(0)),
+                                             range(0, self.kernel_size[0] * self.dilation[0], self.dilation[0]),
+                                             range(0, self.kernel_size[1] * self.dilation[1], self.dilation[1])):
+                KQI.kqi += self.KQI_formula(volume[0] / np.prod(self.kernel_size), volume_backward[c, i:H * self.stride[0] + i:self.stride[0], j:W * self.stride[1] + j:self.stride[1]])
+
+        logging.debug(f'MaxPool2d: KQI={KQI.kqi}, node={np.product(volume.shape)}, volume={volume.sum()}')
+
+        return volume_backward
+
+    def _degree(self, input_size, output_size):
+        _, HI, WI = input_size
+        _, HO, WO = output_size
+
+        degree = torch.zeros(HO, WO)
+        for i, j in itertools.product(range(0, self.kernel_size[0]*self.dilation[0], self.dilation[0]), range(0, self.kernel_size[1]*self.dilation[1], self.dilation[1])):
+            Hleft = max(0, math.ceil((self.padding[0] - i) / self.stride[0]))
+            Hright = min(HO, math.ceil((HI - i + self.padding[0]) / self.stride[0]))
+            Wleft = max(0, math.ceil((self.padding[1] - j) / self.stride[1]))
+            Wright = min(WO, math.ceil((WI - j + self.padding[1]) / self.stride[1]))
+            degree[Hleft:Hright, Wleft:Wright] += 1
+        return degree
---
+++ pytests/test_pooling.py
@@ -0,0 +1,91 @@
+import torch
+import kqinn
+import kqitool
+import itertools
+import logging
+
+
+def test_MaxPool():
+    class TestMaxPool2d(torch.nn.Module, kqinn.KQI):
+        def __init__(self) -> None:
+            super().__init__()
+            self.layers1 = kqinn.Sequential(
+                # 1x28x28
+                kqinn.Conv2d(in_channels=1, out_channels=3, kernel_size=3, bias=False),
+                # 3x26x26
+                kqinn.MaxPool2d(kernel_size=2, stride=2, padding=1, dilation=1)
+            )
+            self.layers2 = kqinn.Sequential(
+                # 3x14x14
+                kqinn.Linear(in_features=3 * 14 * 14, out_features=100, bias=False),
+                kqinn.Linear(in_features=100, out_features=10, bias=False),
+            )
+
+        def forward(self, x):
+            x = self.layers1(x)
+            x = x.flatten()
+            x = self.layers2(x)
+            return x
+
+        def KQIforward(self, x: torch.Tensor) -> torch.Tensor:
+            x = self.layers1.KQIforward(x)
+            x = x.flatten()
+            x = self.layers2.KQIforward(x)
+            return x
+
+        def KQIbackward(self, volume: torch.Tensor, volume_backward: torch.Tensor = None) -> torch.Tensor:
+            volume = self.layers2.KQIbackward(volume)
+            volume = volume.reshape(3, 14, 14)
+            volume = self.layers1.KQIbackward(volume, volume_backward)
+
+            return volume
+
+        def true_kqi(self):
+            G = kqitool.DiGraph()
+            for i, j in itertools.product(range(28), range(28)):
+                G.add_node(f'L1_{i}-{j}', [])
+            for i, j in itertools.product(range(26), range(26)):
+                preds = [f'L1_{k1}-{k2}' for k1, k2 in itertools.product([i, i+1, i+2], [j, j+1, j+2])]
+                G.add_node(f'L2_{i}-{j}_1', preds)
+                G.add_node(f'L2_{i}-{j}_2', preds)
+                G.add_node(f'L2_{i}-{j}_3', preds)
+            for i, j in itertools.product(range(14), range(14)):
+                for k3 in [1, 2, 3]:
+                    preds = [f'L2_{k1}-{k2}_{k3}' for k1, k2 in itertools.product([i*2-1, i*2], [j*2-1, j*2])
+                             if 0 <= k1 < 26 and 0 <= k2 < 26]
+                    G.add_node(f'L3_{i}-{j}_{k3}', preds)
+
+            for i in range(100):
+                preds = [f'L3_{k1}-{k2}_{k3}' for k1, k2 in itertools.product(range(14), range(14)) for k3 in [1, 2, 3]]
+                G.add_node(f'L4_{i}', preds)
+            for i in range(10):
+                preds = [f'L4_{k}' for k in range(100)]
+                G.add_node(f'L5_{i}', preds)
+
+            kqi = sum(map(lambda k: G.kqi(k) if "L5_" in k else 0, G.nodes()))
+            logging.debug(
+                f'L5: KQI={kqi}, node={len([k for k in G.nodes() if "L5_" in k])}, volume={sum([G.volume(k) for k in G.nodes() if "L5_" in k])}')
+            kqi += sum(map(lambda k: G.kqi(k) if "L4_" in k else 0, G.nodes()))
+            logging.debug(
+                f'L4: KQI={kqi}, node={len([k for k in G.nodes() if "L4_" in k])}, volume={sum([G.volume(k) for k in G.nodes() if "L4_" in k])}')
+            kqi += sum(map(lambda k: G.kqi(k) if "L3_" in k else 0, G.nodes()))
+            logging.debug(
+                f'L3: KQI={kqi}, node={len([k for k in G.nodes() if "L3_" in k])}, volume={sum([G.volume(k) for k in G.nodes() if "L3_" in k])}')
+            kqi += sum(map(lambda k: G.kqi(k) if "L2_" in k else 0, G.nodes()))
+            logging.debug(
+                f'L2: KQI={kqi}, node={len([k for k in G.nodes() if "L2_" in k])}, volume={sum([G.volume(k) for k in G.nodes() if "L2_" in k])}')
+            kqi += sum(map(lambda k: G.kqi(k) if "L1_" in k else 0, G.nodes()))
+            logging.debug(
+                f'L1: KQI={kqi}, node={len([k for k in G.nodes() if "L1_" in k])}, volume={sum([G.volume(k) for k in G.nodes() if "L1_" in k])}')
+            logging.debug(f'Total volume = {G.graph_volume()}')
+
+            return sum(map(lambda k: G.kqi(k), G.nodes()))
+
+    kqi = TestMaxPool2d().KQI(torch.randn(1, 28, 28))
+    true = TestMaxPool2d().true_kqi()
+    logging.debug(f'KQI = {kqi} (True KQI = {true})')
+    assert abs(kqi - true) / true < 0.0001
+
+
+if __name__ == '__main__':
+    test_MaxPool()
