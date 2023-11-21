f5ca01c7bb288235726ca81f66f987120a679323
simonynwa <dingyuang@sjtu.edu.cn>
2023-11-21 12:43:55 +0800

    dropout

--- kqinn/dropout.py
+++ kqinn/dropout.py
@@ -63,3 +63,15 @@ class AlphaDropout(torch.nn.Dropout, KQI):
 
         logging.debug(f'AlphaDropout: KQI={KQI.kqi}, node={np.prod(volume.shape)}, volume={volume.sum()}')
         return volume_backward
+
+
+class FeatureAlphaDropout(torch.nn.Dropout, KQI):
+    def KQIforward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.forward(x)
+
+    def KQIbackward(self, volume: torch.Tensor, volume_backward: torch.Tensor = None) -> torch.Tensor:
+        if volume_backward is None:
+            volume_backward = volume
+
+        logging.debug(f'FeatureAlphaDropout: KQI={KQI.kqi}, node={np.prod(volume.shape)}, volume={volume.sum()}')
+        return volume_backward
--- pytests/test_dropout.py
+++ pytests/test_dropout.py
@@ -328,9 +328,82 @@ def test_AlphaDropout():
     assert abs(kqi - true) / true < 0.0001
 
 
+def test_FeatureAlphaDropout():
+    class TestFeatureAlphaDropout(torch.nn.Module, kqinn.KQI):
+        def __init__(self) -> None:
+            super().__init__()
+            self.layers1 = kqinn.Sequential(
+                # 1x9x9x9
+                kqinn.Conv3d(in_channels=3, out_channels=2, kernel_size=3, stride=1, padding=1, dilation=1, bias=False),
+                kqinn.FeatureAlphaDropout(p=0.4, inplace=True),
+                kqinn.Conv3d(in_channels=2, out_channels=3, kernel_size=3, stride=3, padding=0, dilation=2, bias=False),
+                kqinn.FeatureAlphaDropout(p=0.5, inplace=True),
+            )
+            self.layers2 = kqinn.Sequential(
+                # 3x2x2x2
+                kqinn.Linear(in_features=3*2*2*2, out_features=100, bias=False),
+                kqinn.Linear(in_features=100, out_features=10, bias=False),
+            )
+
+        def forward(self, x):
+            x = self.layers1(x)
+            x = x.flatten()
+            x = self.layers2(x)
+
+            return x
+
+        def KQIforward(self, x: torch.Tensor) -> torch.Tensor:
+            x = self.layers1.KQIforward(x)
+            x = x.flatten()
+            x = self.layers2.KQIforward(x)
+
+            return x
+
+        def KQIbackward(self, volume: torch.Tensor, volume_backward: torch.Tensor = None) -> torch.Tensor:
+            volume = self.layers2.KQIbackward(volume)
+            volume = volume.reshape(3, 2, 2, 2)
+            volume = self.layers1.KQIbackward(volume, volume_backward)
+
+            return volume
+
+        def true_kqi(self):
+            G = kqitool.DiGraph()
+            for i, j, k in itertools.product(range(9), range(9), range(9)):
+                G.add_node(f'L1_{i}-{j}-{k}_1', [])
+                G.add_node(f'L1_{i}-{j}-{k}_2', [])
+                G.add_node(f'L1_{i}-{j}-{k}_3', [])
+
+            for i, j, k in itertools.product(range(9), range(9), range(9)):
+                preds = [f'L1_{m1}-{m2}-{m3}_{m4}' for m1, m2, m3 in itertools.product([i-1, i, i+1], [j-1, j, j+1], [k-1, k, k+1]) if m1 >= 0 and m1 < 9 and m2 >= 0 and m2 < 9 and m3 >= 0 and m3 < 9 for m4 in [1, 2, 3]]
+                G.add_node(f'L2_{i}-{j}-{k}_1', preds)
+                G.add_node(f'L2_{i}-{j}-{k}_2', preds)
+
+            for i, j, k in itertools.product(range(2), range(2), range(2)):
+                preds = [f'L2_{m1}-{m2}-{m3}_{m4}' for m1, m2, m3 in itertools.product([i*3, i*3+2, i*3+4], [j*3, j*3+2, j*3+4], [k*3, k*3+2, k*3+4]) for m4 in [1, 2]]
+                G.add_node(f'L3_{i}-{j}-{k}_1', preds)
+                G.add_node(f'L3_{i}-{j}-{k}_2', preds)
+                G.add_node(f'L3_{i}-{j}-{k}_3', preds)
+
+            for i in range(100):
+                preds = [f'L3_{m1}-{m2}-{m3}_{m4}' for m1, m2, m3 in itertools.product(range(2), range(2), range(2)) for m4 in [1, 2, 3]]
+                G.add_node(f'L4_{i}', preds)
+
+            for i in range(10):
+                preds = [f'L4_{m}' for m in range(100)]
+                G.add_node(f'L5_{i}', preds)
+
+            return sum(map(lambda k: G.kqi(k), G.nodes()))
+
+    kqi = TestFeatureAlphaDropout().KQI(torch.randn(3, 9, 9, 9))
+    true = TestFeatureAlphaDropout().true_kqi()
+    logging.debug(f'KQI = {kqi} (True KQI = {true})')
+    assert abs(kqi - true) / true < 0.0001
+
+
 if __name__ == '__main__':
     test_Dropout()
     test_Dropout1d()
     test_Dropout2d()
     test_Dropout3d()
     test_AlphaDropout()
+    test_FeatureAlphaDropout()
