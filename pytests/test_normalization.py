db6213eabb3a10a5a81966313b6a4e3afbd65053
simonynwa <dingyuang@sjtu.edu.cn>
2023-12-06 23:04:53 +0800

    blank

--- pytests/test_normalization.py
+++ pytests/test_normalization.py
@@ -96,13 +96,13 @@ def test_GroupNorm():
         def __init__(self) -> None:
             super().__init__()
 
-            self.layer1 = kqinn.GroupNorm(num_groups=3, num_channels=6, affine = False)
+            self.layer1 = kqinn.GroupNorm(num_groups=3, num_channels=6, affine=False)
             self.layer2 = kqinn.Linear(in_features=1*6*10*10, out_features=1*6*10*10,
                                        bias=False)
-            self.layer3 = kqinn.GroupNorm(num_groups=2, num_channels=6, affine = False)
+            self.layer3 = kqinn.GroupNorm(num_groups=2, num_channels=6, affine=False)
             self.layer4 = kqinn.Linear(in_features=1*6*10*10, out_features=1*6*10*10,
                                        bias=False)
-            self.layer5 = kqinn.GroupNorm(num_groups=1, num_channels=6, affine = False)
+            self.layer5 = kqinn.GroupNorm(num_groups=1, num_channels=6, affine=False)
 
         def forward(self, x):
             x = self.layer1(x)
@@ -136,14 +136,14 @@ def test_GroupNorm():
 
             # Separate 6 channels into 3 groups
             preds1 = [f'L0_{i}-{j}-{k}' for i, j, k in itertools.product(range(10), range(10), range(2))]
-            preds2 = [f'L0_{i}-{j}-{k}' for i, j, k in itertools.product(range(10), range(10), range(2,4))]
-            preds3 = [f'L0_{i}-{j}-{k}' for i, j, k in itertools.product(range(10), range(10), range(4,6))]
+            preds2 = [f'L0_{i}-{j}-{k}' for i, j, k in itertools.product(range(10), range(10), range(2, 4))]
+            preds3 = [f'L0_{i}-{j}-{k}' for i, j, k in itertools.product(range(10), range(10), range(4, 6))]
             for i, j in itertools.product(range(10), range(10)):
                 for k in range(2):
                     G.add_node(f'L1_{i}-{j}-{k}', preds1)
-                for k in range(2,4):
+                for k in range(2, 4):
                     G.add_node(f'L1_{i}-{j}-{k}', preds2)
-                for k in range(4,6):
+                for k in range(4, 6):
                     G.add_node(f'L1_{i}-{j}-{k}', preds3)
 
             # Linear
@@ -153,11 +153,11 @@ def test_GroupNorm():
 
             # Separate 6 channels into 2 groups
             preds1 = [f'L2_{i}-{j}-{k}' for i, j, k in itertools.product(range(10), range(10), range(3))]
-            preds2 = [f'L2_{i}-{j}-{k}' for i, j, k in itertools.product(range(10), range(10), range(3,6))]
+            preds2 = [f'L2_{i}-{j}-{k}' for i, j, k in itertools.product(range(10), range(10), range(3, 6))]
             for i, j in itertools.product(range(10), range(10)):
                 for k in range(3):
                     G.add_node(f'L3_{i}-{j}-{k}', preds1)
-                for k in range(3,6):
+                for k in range(3, 6):
                     G.add_node(f'L3_{i}-{j}-{k}', preds2)
 
             # Linear
@@ -221,7 +221,7 @@ def test_LocalResponseNorm():
             for i, j, k in itertools.product(range(10), range(10), range(6)):
                 G.add_node(f'L0_{i}-{j}-{k}', [])
 
-            #LocalResponseNorm(3)
+            # LocalResponseNorm(3)
             for i, j, k in itertools.product(range(10), range(10), range(6)):
                 preds = [f'L0_{i}-{j}-{m}' for m in range(max(0,k-1),min(5,k+1)+1)]
                 G.add_node(f'L1_{i}-{j}-{k}', preds)
@@ -231,10 +231,9 @@ def test_LocalResponseNorm():
             for i, j, k in itertools.product(range(10), range(10), range(6)):
                 G.add_node(f'L2_{i}-{j}-{k}', preds)
 
-
-            #LocalResponseNorm(2)
+            # LocalResponseNorm(2)
             for i, j, k in itertools.product(range(10), range(10), range(6)):
-                preds = [f'L2_{i}-{j}-{m}' for m in range(max(0,k-1),min(5,k+1)+1)]
+                preds = [f'L2_{i}-{j}-{m}' for m in range(max(0, k-1),min(5, k+1)+1)]
                 G.add_node(f'L3_{i}-{j}-{k}', preds)
             
             # Linear
@@ -242,24 +241,10 @@ def test_LocalResponseNorm():
             for i, j, k in itertools.product(range(10), range(10), range(6)):
                 G.add_node(f'L4_{i}-{j}-{k}', preds)
 
-
-            #LocalResponseNorm(4)
+            # LocalResponseNorm(4)
             for i, j, k in itertools.product(range(10), range(10), range(6)):
-                preds = [f'L4_{i}-{j}-{m}' for m in range(max(0,k-2),min(5,k+2)+1)]
+                preds = [f'L4_{i}-{j}-{m}' for m in range(max(0, k-2),min(5, k+2)+1)]
                 G.add_node(f'L5_{i}-{j}-{k}', preds)
-
-
-            kqi = sum(map(lambda k: G.kqi(k) if "L5_" in k else 0, G.nodes()))
-            logging.debug(f'L5: KQI={kqi}, node={len([k for k in G.nodes() if "L5_" in k])}, volume={sum([G.volume(k) for k in G.nodes() if "L5_" in k])}')
-            kqi += sum(map(lambda k: G.kqi(k) if "L4_" in k else 0, G.nodes()))
-            logging.debug(f'L4: KQI={kqi}, node={len([k for k in G.nodes() if "L4_" in k])}, volume={sum([G.volume(k) for k in G.nodes() if "L4_" in k])}')
-            kqi += sum(map(lambda k: G.kqi(k) if "L3_" in k else 0, G.nodes()))
-            logging.debug(f'L3: KQI={kqi}, node={len([k for k in G.nodes() if "L3_" in k])}, volume={sum([G.volume(k) for k in G.nodes() if "L3_" in k])}')
-            kqi += sum(map(lambda k: G.kqi(k) if "L2_" in k else 0, G.nodes()))
-            logging.debug(f'L2: KQI={kqi}, node={len([k for k in G.nodes() if "L2_" in k])}, volume={sum([G.volume(k) for k in G.nodes() if "L2_" in k])}')
-            kqi += sum(map(lambda k: G.kqi(k) if "L1_" in k else 0, G.nodes()))
-            logging.debug(f'L1: KQI={kqi}, node={len([k for k in G.nodes() if "L1_" in k])}, volume={sum([G.volume(k) for k in G.nodes() if "L1_" in k])}')
-            logging.debug(f'Total volume = {G.graph_volume()}')
 
             return sum(map(lambda m: G.kqi(m), G.nodes()))
 
