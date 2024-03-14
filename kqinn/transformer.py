ebc7fb1f8b42c280ab2449cf1f299198fb65e75b
Betty Huang <bettyhuang@BettydeMacBook-Air.local>
2024-03-14 20:45:21 +0800

    transformerDecoderLayer modified

--- kqinn/transformer.py
+++ kqinn/transformer.py
@@ -33,6 +33,31 @@ class TransformerEncoder(torch.nn.TransformerEncoder, KQI):
         return volume
 
 
+class TransformerDecoder(torch.nn.TransformerDecoder, KQI):
+    """
+    This module is modified from torch.nn.TransformerEncoder.
+    We only consider the case of norm_first=True.
+    """
+
+    def __init__(self, decoder_layer, num_layers, norm=None, enable_nested_tensor=True, mask_check=True):
+        super().__init__(decoder_layer, num_layers, norm, enable_nested_tensor, mask_check)
+        self.decoder_layer = decoder_layer
+        self.num_layers = num_layers
+
+    def KQIforward(self, x: Tensor, memory: Tensor) -> Tensor:
+        for i in range(self.num_layers):
+            self.decoder_layer.KQIforward(x, memory)
+
+        return self.forward(x, memory)
+
+    def KQIbackward(self, volume: Tensor, volume_backward_x: Tensor = None, volume_backward_memory: Tensor = None,) -> Tensor:
+        volume = torch.zeros_like(volume)
+        for i in range(self.num_layers):
+            volume = self.decoder_layer.KQIbackward(volume)
+
+        return volume
+
+
 class TransformerEncoderLayer(torch.nn.TransformerEncoderLayer, KQI):
     """
     This module is modified from torch.nn.TransformerEncoderLayer.
@@ -62,6 +87,38 @@ class TransformerEncoderLayer(torch.nn.TransformerEncoderLayer, KQI):
         return volume
 
 
+class TransformerDecoderLayer(torch.nn.TransformerDecoderLayer, KQI):
+    """
+    This module is modified from torch.nn.TransformerEncoderLayer.
+    We only consider the case of norm_first=True.
+    """
+
+    def __init__(self, d_model: int, nhead: int, dim_feedforward: int = 2048, dropout: float = 0.1,
+                 norm_first: bool = True):
+        if not norm_first:
+            raise NotImplementedError('norm_first=False is not supported now')
+
+        super().__init__(d_model, nhead, dim_feedforward, dropout, norm_first=norm_first)
+        self.norm_first = norm_first
+        self.sa_block = sa_block(d_model, nhead, dropout)
+        self.mha_block = mha_block(d_model, nhead, dropout)
+        self.ff_block = ff_block(d_model, dim_feedforward, dropout)
+
+    def KQIforward(self, x: Tensor) -> Tensor:
+        x = kqinn.Branch(self.sa_block, kqinn.EmptyModule()).KQIforward(x)
+        x = kqinn.Branch(self.mha_block, kqinn.EmptyModule()).KQIforward(x)
+        x = kqinn.Branch(self.ff_block, kqinn.EmptyModule()).KQIforward(x)
+
+        return self.forward(x)
+
+    def KQIbackward(self, volume: torch.Tensor, volume_backward: torch.Tensor = None) -> torch.Tensor:
+        volume = kqinn.Branch(self.ff_block, kqinn.EmptyModule()).KQIbackward(volume)
+        volume = kqinn.Branch(self.mha_block, kqinn.EmptyModule()).KQIbackward(volume)
+        volume = kqinn.Branch(self.sa_block, kqinn.EmptyModule()).KQIbackward(volume)
+        logging.debug(f'TransformerEncoderLayer: KQI={KQI.kqi}, node={np.prod(volume.shape)}, volume={volume.sum()}')
+        return volume
+
+
 class sa_block(KQI):
     def __init__(self, d_model: int, nhead: int, dropout: float = 0.1):
         self.self_attn = kqinn.MultiheadAttention(d_model, nhead, dropout=dropout)
@@ -86,6 +143,65 @@ class sa_block(KQI):
                                    volume_backward_v=volume_kqv)
         volume = self.norm1.KQIbackward(volume_kqv, volume_backward)
         logging.debug(f'sa_block: KQI={KQI.kqi}, node={np.prod(volume.shape)}, volume={volume.sum()}')
+        return volume
+
+
+# class mha_block(KQI):
+#     def __init__(self, d_model: int, nhead: int, dropout: float = 0.1):
+#         self.self_attn = kqinn.MultiheadAttention(d_model, nhead, dropout=dropout)
+#         self.norm1 = kqinn.LayerNorm(d_model)
+#         self.dropout1 = kqinn.Dropout(dropout)
+#
+#     def KQIforward(self, x: Tensor) -> Tensor:
+#         x = self.norm1.KQIforward(x)
+#         x = self.self_attn.KQIforward(x, x, x)[0]
+#         x = self.dropout1.KQIforward(x)
+#
+#         return x
+#
+#     def KQIbackward(self, volume: torch.Tensor, volume_backward: torch.Tensor = None) -> torch.Tensor:
+#         volume = self.dropout1.KQIbackward(volume)
+#
+#         tmp_kqi = KQI.kqi.clone()
+#         volume_backward_q, volume_backward_k, volume_backward_v = self.self_attn.KQIbackward(volume)
+#         volume_kqv = volume_backward_q + volume_backward_k + volume_backward_v
+#         KQI.kqi = tmp_kqi
+#         self.self_attn.KQIbackward(volume=volume, volume_backward_k=volume_kqv, volume_backward_q=volume_kqv,
+#                                    volume_backward_v=volume_kqv)
+#         volume = self.norm1.KQIbackward(volume_kqv, volume_backward)
+#         logging.debug(f'mha_block: KQI={KQI.kqi}, node={np.prod(volume.shape)}, volume={volume.sum()}')
+#         return volume
+
+
+class mha_block(KQI):
+    def __init__(self, d_model: int, nhead: int, dropout: float = 0.1):
+        self.self_attn = kqinn.MultiheadAttention(d_model, nhead, dropout=dropout)
+        self.norm1 = kqinn.LayerNorm(d_model)
+        self.dropout1 = kqinn.Dropout(dropout)
+
+    def KQIforward(self, x: Tensor, memory: Tensor) -> Tensor:
+        x = self.norm1.KQIforward(x)
+        x = self.self_attn.KQIforward(x, memory, memory)[0]
+        x = self.dropout1.KQIforward(x)
+
+        return x
+
+    def KQIbackward(self, volume: torch.Tensor, volume_backward_x: torch.Tensor = None, volume_backward_memory: torch.Tensor = None) -> torch.Tensor:
+        volume = self.dropout1.KQIbackward(volume)
+
+        tmp_kqi = KQI.kqi.clone()
+        volume_backward_k, volume_backward_q, volume_backward_v = self.self_attn.KQIbackward(volume)
+        volume_x = volume_backward_q
+        volume_memory = volume_backward_k + volume_backward_v
+        KQI.kqi = tmp_kqi
+        if volume_backward_memory is None:
+            self.self_attn.KQIbackward(volume=volume, volume_backward_k=volume_memory, volume_backward_q=volume_x,
+                                   volume_backward_v=volume_memory)
+        else:
+            self.self_attn.KQIbackward(volume=volume, volume_backward_k=volume_backward_memory, volume_backward_q=volume_x,
+                                   volume_backward_v=volume_backward_memory)
+        volume = self.norm1.KQIbackward(volume_x, volume_backward_x)
+        logging.debug(f'mha_block: KQI={KQI.kqi}, node={np.prod(volume.shape)}, volume={volume.sum()}')
         return volume
 
 
--- pytests/test_transformer.py
+++ pytests/test_transformer.py
@@ -353,6 +353,216 @@ def test_TransformerEncoderLayer():
     assert abs(kqi - true) / true < 0.0001
 
 
+
+def test_TransformerDecoderLayer():
+    batch_size = 1
+    sequence_length = 1
+    d_model = 512
+    head = 8
+    dim_feedforward = 48
+
+    class TestTransformerDecoderLayer(torch.nn.Module, kqinn.KQI):
+        def __init__(self) -> None:
+            super().__init__()
+            self.layer = kqinn.TransformerDecoderLayer(d_model=d_model, nhead=head, dim_feedforward=dim_feedforward, norm_first=True)
+            self.memory = None
+
+        def set_memory(self, memory):
+            self.memory = memory
+
+        def forward(self, x):
+            return self.layer(x, self.memory)
+
+        def KQIforward(self, x: torch.Tensor) -> torch.Tensor:
+            x = x.flatten()
+            tgt_mem = self.layer.KQIforward(x).reshape(sequence_length, d_model * 2)
+            tgt = tgt_mem[:, :d_model].reshape(sequence_length, d_model)
+            mem = tgt_mem[:, d_model:].reshape(sequence_length, d_model)
+            return self.layer.KQIforward(tgt, mem)
+
+        def KQIbackward(self, volume: torch.Tensor, volume_backward_x: torch.Tensor = None, volume_backward_memory: torch.Tensor = None) -> torch.Tensor:
+            return self.layer.KQIbackward(volume, volume_backward_x)
+
+        def true_kqi(self):
+            G = kqitool.DiGraph()
+
+            # Define Memory nodes representing the encoder output
+            for i in range(sequence_length):
+                for k in range(d_model):
+                    G.add_node(f'M_{i}-{k}', [])  # No predecessors for encoder output nodes
+
+            for i in range(d_model):
+                G.add_node(f'L0_{i}', [])
+
+            # Norm1
+            preds = [f'L0_{i}' for i in range(d_model)]
+            for i in range(sequence_length):
+                for j in range(d_model):
+                    G.add_node(f'L1_{i}-{j}', preds)
+
+            # ------------------------- SelfAttention -------------------------
+            embedding_dim = d_model
+            head_dim = embedding_dim // head
+            # linear
+            for i in range(head):
+                predsQ = [f'L1_{j}-{k}' for j, k in
+                          itertools.product(range(sequence_length), range(i * head_dim, (i + 1) * head_dim))]
+                predsK = [f'L1_{j}-{k}' for j, k in
+                          itertools.product(range(sequence_length), range(i * head_dim, (i + 1) * head_dim))]
+                predsV = [f'L1_{j}-{k}' for j, k in
+                          itertools.product(range(sequence_length), range(i * head_dim, (i + 1) * head_dim))]
+                for j, k in itertools.product(range(sequence_length), range(i * head_dim, (i + 1) * head_dim)):
+                    G.add_node(f'L2_Q_{j}-{k}', predsQ)
+                    G.add_node(f'L2_K_{j}-{k}', predsK)
+                    G.add_node(f'L2_V_{j}-{k}', predsV)
+            # MatMul
+            for i in range(head):
+                for j in range(sequence_length):
+                    for k in range(sequence_length):
+                        preds = ([f'L2_Q_{j}-{i * head_dim + m}' for m in range(head_dim)]
+                                 + [f'L2_K_{k}-{i * head_dim + m}' for m in range(head_dim)])
+                        G.add_node(f'L3_{j}-{i * sequence_length + k}', preds)
+            # Scale
+            for i in range(head):
+                for j in range(sequence_length):
+                    for k in range(sequence_length):
+                        preds = [f'L3_{j}-{i * sequence_length + k}']
+                        G.add_node(f'L4_{j}-{i * sequence_length + k}', preds)
+            # # Mask
+            # for i in range(head):
+            #     for j in range(sequence_length):
+            #         for k in range(sequence_length):
+            #             preds = [f'L4_{j}-{i * sequence_length + k}']
+            #             G.add_node(f'L5_{j}-{i * sequence_length + k}', preds)
+            # SoftMax
+            for i in range(head):
+                preds = [f'L4_{j}-{i * sequence_length + k}' for j, k in
+                         itertools.product(range(sequence_length), range(sequence_length))]
+                for j, k in itertools.product(range(sequence_length), range(sequence_length)):
+                    G.add_node(f'L6_{j}-{i * sequence_length + k}', preds)
+            # MatMul
+            for i in range(head):
+                for j in range(sequence_length):
+                    for k in range(head_dim):
+                        preds = ([f'L6_{j}-{i * sequence_length + m}' for m in range(sequence_length)] +
+                                 [f'L2_V_{m}-{i * head_dim + k}' for m in range(sequence_length)])
+                        G.add_node(f'L7_{j}-{i * head_dim + k}', preds)
+            # Linear
+            preds = [f'L7_{j}-{k}' for j, k in itertools.product(range(sequence_length), range(embedding_dim))]
+            for j, k in itertools.product(range(sequence_length), range(embedding_dim)):
+                G.add_node(f'L8_{j}-{k}', preds)
+
+            # ------------------------- SelfAttention -------------------------
+
+            # Add
+            for i in range(sequence_length):
+                for j in range(d_model):
+                    preds = ([f'L0_{j}'] + [f'L8_{i}-{j}'])
+                    G.add_node(f'L9_{i}-{j}', preds)
+
+            # Norm2
+            preds = [f'L9_{i}-{j}' for i, j in itertools.product(range(sequence_length), range(embedding_dim))]
+            # resize L10
+            for i in range(d_model): ##
+                for j in range(embedding_dim):
+                    G.add_node(f'L10_{i}-{j}', preds)
+
+            # ------------------------- MultiheadAttention -------------------------
+            # embedding_dim = d_model
+            # head_dim = embedding_dim // head
+            # linear
+            for i in range(head):
+                predsQ = [f'L10_{j}-{k}' for j, k in
+                          itertools.product(range(sequence_length), range(embedding_dim))] ##
+                predsK = [f'M_{j}-{k}' for j, k in  # These preds come from the Memory (encoder output)
+                          itertools.product(range(sequence_length), range(i * head_dim, (i + 1) * head_dim))]
+                predsV = [f'M_{j}-{k}' for j, k in  # These preds come from the Memory (encoder output)
+                          itertools.product(range(sequence_length), range(i * head_dim, (i + 1) * head_dim))]
+                for j, k in itertools.product(range(sequence_length), range(i * head_dim, (i + 1) * head_dim)):
+                    G.add_node(f'L11_Q_{j}-{k}', predsQ)
+                    G.add_node(f'L11_K_{j}-{k}', predsK)
+                    G.add_node(f'L11_V_{j}-{k}', predsV)
+            # MatMul
+            for i in range(head):
+                for j in range(sequence_length):
+                    for k in range(sequence_length):
+                        preds = ([f'L11_Q_{j}-{i * head_dim + m}' for m in range(head_dim)]
+                                 + [f'L11_K_{k}-{i * head_dim + m}' for m in range(head_dim)])
+                        G.add_node(f'L12_{j}-{i * sequence_length + k}', preds)
+            # Scale
+            for i in range(head):
+                for j in range(sequence_length):
+                    for k in range(sequence_length):
+                        preds = [f'L12_{j}-{i * sequence_length + k}']
+                        G.add_node(f'L13_{j}-{i * sequence_length + k}', preds)
+            # Mask
+            # for i in range(head):
+            #     for j in range(sequence_length):
+            #         for k in range(sequence_length):
+            #             preds = [f'L13_{j}-{i * sequence_length + k}']
+            #             G.add_node(f'L14_{j}-{i * sequence_length + k}', preds)
+            # SoftMax
+            for i in range(head):
+                preds = [f'L13_{j}-{i * sequence_length + k}' for j, k in
+                         itertools.product(range(sequence_length), range(sequence_length))]
+                for j, k in itertools.product(range(sequence_length), range(sequence_length)):
+                    G.add_node(f'L15_{j}-{i * sequence_length + k}', preds)
+            # MatMul
+            for i in range(head):
+                for j in range(sequence_length):
+                    for k in range(head_dim):
+                        preds = ([f'L15_{j}-{i * sequence_length + m}' for m in range(sequence_length)] +
+                                 [f'L11_V_{m}-{i * head_dim + k}' for m in range(sequence_length)])
+                        G.add_node(f'L16_{j}-{i * head_dim + k}', preds)
+            # Linear
+            preds = [f'L16_{j}-{k}' for j, k in itertools.product(range(sequence_length), range(embedding_dim))]
+            for j, k in itertools.product(range(sequence_length), range(embedding_dim)):
+                G.add_node(f'L17_{j}-{k}', preds)
+
+            # ------------------------- MultiheadAttention -------------------------
+
+            # Add
+            for i in range(sequence_length):
+                for j in range(d_model):
+                    preds = ([f'L9_{i}-{j}'] + [f'L17_{i}-{j}'])
+                    G.add_node(f'L18_{j}', preds)
+
+            # Norm3
+            preds = [f'L18_{i}' for i in range(d_model)]
+            for i in range(d_model):
+                G.add_node(f'L19_{i}', preds)
+
+            # Linear1
+            preds = [f'L19_{i}' for i in range(d_model)]
+            for i in range(dim_feedforward):
+                G.add_node(f'L20_{i}', preds)
+
+            # Linear2
+            preds = [f'L20_{i}' for i in range(dim_feedforward)]
+            for i in range(d_model):
+                G.add_node(f'L21_{i}', preds)
+
+            # Add
+            for i in range(d_model):
+                preds = ([f'L18_{i}'] + [f'L21_{i}'])
+                G.add_node(f'L22_{i}', preds)
+
+            return sum(map(lambda m: G.kqi(m), G.nodes()))
+
+    tgt = torch.randn(batch_size, sequence_length, d_model)
+    memory = torch.rand(batch_size, sequence_length, d_model)
+    decoder_layer = TestTransformerDecoderLayer()
+    decoder_layer.set_memory(memory)
+    kqi = decoder_layer.KQI(tgt)
+    true = decoder_layer.true_kqi()
+
+    print("true_kqi: ", true)
+    print("kqi: ", kqi)
+    logging.debug(f'KQI = {kqi} (True KQI = {true})')
+    assert abs(kqi - true) / true < 0.0001
+
+
 if __name__ == '__main__':
     test_TransformerEncoder()
     test_TransformerEncoderLayer()
+    test_TransformerDecoderLayer()
