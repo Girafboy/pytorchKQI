fd14507ded30f9d23ce011d67ff783defd780ab4
Huquan Kang <799385272@qq.com>
2024-07-22 12:06:10 +0800

    tidy up

--- demo.py
+++ demo.py
@@ -1,32 +1,33 @@
 import torch
+import torchvision
 import torchKQI
 import pandas as pd
-from torchvision.models import *
 import os
+
 
 def task_ImageClassification():
     x = torch.randn(1, 3, 224, 224)
-    
-    model_names = [
-        'alexnet',
-        'convnext_tiny', 'convnext_small', 'convnext_base', 'convnext_large',
-        'densenet121', 'densenet161', 'densenet169', 'densenet201',
-        "efficientnet_b0","efficientnet_b1","efficientnet_b2","efficientnet_b3","efficientnet_b4","efficientnet_b5","efficientnet_b6","efficientnet_b7","efficientnet_v2_s","efficientnet_v2_m","efficientnet_v2_l",
-        'googlenet',
-        'inception_v3',
-        "maxvit_t",
-        "mnasnet0_5","mnasnet0_75","mnasnet1_0","mnasnet1_3",
-        'mobilenet_v2',
-        "mobilenet_v3_large","mobilenet_v3_small",
-        "regnet_y_400mf","regnet_y_800mf","regnet_y_1_6gf","regnet_y_3_2gf","regnet_y_8gf","regnet_y_16gf","regnet_y_32gf","regnet_y_128gf","regnet_x_400mf","regnet_x_800mf","regnet_x_1_6gf","regnet_x_3_2gf","regnet_x_8gf","regnet_x_16gf","regnet_x_32gf",
-        'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152',
-        "resnext50_32x4d", "resnext101_32x8d", "resnext101_64x4d",
-        "wide_resnet50_2", "wide_resnet101_2",
-        'shufflenet_v2_x0_5', 'shufflenet_v2_x1_0', 'shufflenet_v2_x1_5', 'shufflenet_v2_x2_0',
-        "squeezenet1_0", "squeezenet1_1",
-        "swin_t","swin_s","swin_b","swin_v2_t","swin_v2_s","swin_v2_b",
-        "vgg11","vgg11_bn","vgg13","vgg13_bn","vgg16","vgg16_bn","vgg19","vgg19_bn",
-        'vit_b_16', 'vit_b_32', 'vit_l_16', 'vit_l_32', 'vit_h_14'
+
+    model_fns = [
+        torchvision.models.alexnet,
+        torchvision.models.convnext_tiny, torchvision.models.convnext_small, torchvision.models.convnext_base, torchvision.models.convnext_large,
+        torchvision.models.densenet121, torchvision.models.densenet161, torchvision.models.densenet169, torchvision.models.densenet201,
+        torchvision.models.efficientnet_b0, torchvision.models.efficientnet_b1, torchvision.models.efficientnet_b2, torchvision.models.efficientnet_b3, torchvision.models.efficientnet_b4, torchvision.models.efficientnet_b5, torchvision.models.efficientnet_b6, torchvision.models.efficientnet_b7, torchvision.models.efficientnet_v2_s, torchvision.models.efficientnet_v2_m, torchvision.models.efficientnet_v2_l,
+        torchvision.models.googlenet,
+        torchvision.models.inception_v3,
+        # torchvision.models.maxvit_t,
+        torchvision.models.mnasnet0_5, torchvision.models.mnasnet0_75, torchvision.models.mnasnet1_0, torchvision.models.mnasnet1_3,
+        torchvision.models.mobilenet_v2,
+        torchvision.models.mobilenet_v3_large, torchvision.models.mobilenet_v3_small,
+        torchvision.models.regnet_y_400mf, torchvision.models.regnet_y_800mf, torchvision.models.regnet_y_1_6gf, torchvision.models.regnet_y_3_2gf, torchvision.models.regnet_y_8gf, torchvision.models.regnet_y_16gf, torchvision.models.regnet_y_32gf, torchvision.models.regnet_y_128gf, torchvision.models.regnet_x_400mf, torchvision.models.regnet_x_800mf, torchvision.models.regnet_x_1_6gf, torchvision.models.regnet_x_3_2gf, torchvision.models.regnet_x_8gf, torchvision.models.regnet_x_16gf, torchvision.models.regnet_x_32gf,
+        torchvision.models.resnet18, torchvision.models.resnet34, torchvision.models.resnet50, torchvision.models.resnet101, torchvision.models.resnet152,
+        torchvision.models.resnext50_32x4d, torchvision.models.resnext101_32x8d, torchvision.models.resnext101_64x4d,
+        torchvision.models.wide_resnet50_2, torchvision.models.wide_resnet101_2,
+        torchvision.models.shufflenet_v2_x0_5, torchvision.models.shufflenet_v2_x1_0, torchvision.models.shufflenet_v2_x1_5, torchvision.models.shufflenet_v2_x2_0,
+        torchvision.models.squeezenet1_0, torchvision.models.squeezenet1_1,
+        torchvision.models.swin_t, torchvision.models.swin_s, torchvision.models.swin_b,  # torchvision.models.swin_v2_t, torchvision.models.swin_v2_s, torchvision.models.swin_v2_b,
+        torchvision.models.vgg11, torchvision.models.vgg11_bn, torchvision.models.vgg13, torchvision.models.vgg13_bn, torchvision.models.vgg16, torchvision.models.vgg16_bn, torchvision.models.vgg19, torchvision.models.vgg19_bn,
+        torchvision.models.vit_b_16, torchvision.models.vit_b_32, torchvision.models.vit_l_16, torchvision.models.vit_l_32, torchvision.models.vit_h_14
     ]
 
     results_file = 'model_results.csv'
@@ -37,17 +38,16 @@ def task_ImageClassification():
     if not os.path.exists(errors_file):
         pd.DataFrame(columns=['Model Name', 'Error']).to_csv(errors_file, index=False)
 
-    for model_name in model_names:
-        if model_name in pd.read_csv(results_file)['Model Name'].values:
+    for model_fn in model_fns:
+        if model_fn.__name__ in pd.read_csv(results_file)['Model Name'].values:
             continue
         try:
-            model_fn = globals()[model_name]
             model = model_fn().eval()
             kqi = torchKQI.KQI(model, x).item()
-            result = pd.DataFrame([[model_name, kqi]], columns=['Model Name', 'KQI'])
+            result = pd.DataFrame([[model_fn.__name__, kqi]], columns=['Model Name', 'KQI'])
             result.to_csv(results_file, mode='a', header=False, index=False)
         except Exception as e:
-            error = pd.DataFrame([[model_name, str(e)]], columns=['Model Name', 'Error'])
+            error = pd.DataFrame([[model_fn.__name__, str(e)]], columns=['Model Name', 'Error'])
             error.to_csv(errors_file, mode='a', header=False, index=False)
 
 
