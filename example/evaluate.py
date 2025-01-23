e18acc634f30794a22598588792fe672718412c8
Huquan Kang <kinghiqian@mail.sjtu.edu.cn>
2025-01-23 14:46:37 +0800

    Update evaluate.py

--- example/evaluate.py
+++ example/evaluate.py
@@ -945,7 +945,6 @@ def task_ObjectDetection(args):
         torchvision.models.detection.fasterrcnn_resnet50_fpn, torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn, torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn, torchvision.models.detection.fasterrcnn_resnet50_fpn_v2,
         torchvision.models.detection.fcos_resnet50_fpn,
         torchvision.models.detection.retinanet_resnet50_fpn, torchvision.models.detection.retinanet_resnet50_fpn_v2,
-        # torchvision.models.detection.ssd300_vgg16,
         torchvision.models.detection.ssdlite320_mobilenet_v3_large,
     ]
 
