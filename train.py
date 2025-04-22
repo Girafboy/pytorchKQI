4a8c520c6b7dd154ff6d4d07522ebff7c700e738
simonynwa <dingyuang@sjtu.edu.cn>
2025-04-22 16:08:06 +0800

    'fix-train-bug'

--- train.py
+++ train.py
@@ -763,5 +763,6 @@ if __name__ == "__main__":
     for num in [1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000, 512000, 1024000]:
         top1_accuracy, top5_accuracy = main(args, num)
         result = (num, top1_accuracy, top5_accuracy)
-        df = pd.DataFrame([result], columns=["Num", "Top-1 Accuracy", "Top-5 Accuracy"])
-        df.to_csv(result_file, mode='a', header=False, index=False)
+        if torch.distributed.get_rank() == 0:
+            df = pd.DataFrame([result], columns=["Num", "Top-1 Accuracy", "Top-5 Accuracy"])
+            df.to_csv(result_file, mode='a', header=False, index=False)
